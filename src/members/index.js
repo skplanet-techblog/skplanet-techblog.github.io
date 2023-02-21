868ff98e5c73ad44dcc61fa7abbec21d93d53251
Joseph Yang <nocoder100@Josephs-MacBook-Pro.local>
2023-02-21 14:06:56 +0900

    샘플 포스팅 추가

--- src/members/index.js
+++ src/members/index.js
@@ -1,4 +1,5 @@
 import jayoon from "./images/jayoon.jpg";
+import hdy from "./images/hdy.png";
 
 export const MEMBERS = {
   jayoon: {
@@ -7,5 +8,11 @@ export const MEMBERS = {
     description: "글쓰기를 좋아하는 프론트엔드 개발자입니다.",
     thumbnail: jayoon,
     link: "https://jayoon-kong.github.io",
+  },
+  hdy: {
+    name: "한대영",
+    team: "블록체인플랫폼개발팀",
+    description: "web3 전문가입니다.",
+    thumbnail: hdy
   },
 };
