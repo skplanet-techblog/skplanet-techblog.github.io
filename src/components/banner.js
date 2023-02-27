d09b1375a9e290594044c1d5e9da09b77f645836
jayoon-kong <jayoon.kong@sk.com>
2023-02-27 20:04:02 +0900

    폰트 사이즈 수정

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,7 +1,16 @@
 import * as React from "react";
+import mobile_banner from "../images/banner_mobile_2x.png";
+import tab_banner from "../images/banner_800_2x.png";
+import pc_banner from "../images/banner_1200_2x.png";
+import full_banner from "../images/banner_full_2x.png";
 
 const Banner = () => (
-  <div className="banner" />
+  <div className="banner">
+    <img src={mobile_banner} alt="mobile_banner" className="mobile" />
+    <img src={tab_banner} alt="tab_banner" className="tab" />
+    <img src={pc_banner} alt="pc_banner" className="pc" />
+    <img src={full_banner} alt="full_banner" className="full" />
+  </div>
 );
 
 export default Banner;
