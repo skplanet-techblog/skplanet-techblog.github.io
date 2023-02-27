d401c844c9b45fd500f164bc9cf695df159b7c43
jayoon-kong <jayoon.kong@sk.com>
2023-02-27 18:15:16 +0900

    정렬 기준 변경

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import Menu from "./menu";
+import logo from "../images/skplanet_2x.png";
 
 const Header = ({ title, location }) => (
   <header className="global-header">
     <div className="main-heading">
       <div className="logo">
+        <img src={logo} alt="logo" />
         <Link to="/">TECH TOPIC</Link>
       </div>
       <Menu location={location} />
