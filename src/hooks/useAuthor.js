e0231407960b6d365099c79a61356d8c576c95ef
공자윤(Jayoon Kong)/Biz플랫폼개발팀/SKP <jayoon.kong@sk.com>
2023-04-10 09:44:28 +0900

    멤버리스트 Name null 처리

--- src/hooks/useAuthor.js
+++ src/hooks/useAuthor.js
@@ -2,17 +2,17 @@ import { MEMBERS } from "../members";
 import thumbnailDefault from "../members/images/default.png";
 
 export function useAuthor(name) {
-  const member = MEMBERS[name.toLowerCase()];
+  const member = MEMBERS[name?.toLowerCase()];
   if (member) {
     return {
       name: `${member.name} (${member.team})`,
       description: member.description,
       thumbnail: member.thumbnail || thumbnailDefault,
-      link: member.link || null
+      link: member.link || null,
     };
   }
   return {
     name,
-    thumbnail: thumbnailDefault
+    thumbnail: thumbnailDefault,
   };
 }
