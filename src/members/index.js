f344a8bbb2843a533fb9440e88e2ecfd0a1fff6c
공자윤(Jayoon Kong)/Biz플랫폼개발팀/SKP <jayoon.kong@sk.com>
2023-07-11 10:46:29 +0900

    블로그 링크 제거

--- src/members/index.js
+++ src/members/index.js
@@ -12,7 +12,6 @@ export const MEMBERS = {
     team: "Biz플랫폼개발팀",
     description: "글쓰기를 좋아하는 프론트엔드 개발자입니다.",
     thumbnail: jayoon,
-    link: "https://jayoon-kong.github.io",
   },
   hdy: {
     name: "한대영",
@@ -33,22 +32,25 @@ export const MEMBERS = {
     thumbnail: hyuna,
   },
   leejun: {
-    name: "이준", 
-    team: "커뮤니케이션기술개발팀", 
-    description: "고객에게 안정적이고 유연한 서비스를 제공하기 위해, 적극적으로 새로운 기술을 도입해 보면서 해답을 찾고 있는 소프트웨어 엔지니어입니다.", 
-    thumbnail: leejun, 
-    link: "https://www.linkedin.com/in/jun-lee-8530b4210", 
-  }, 
+    name: "이준",
+    team: "커뮤니케이션기술개발팀",
+    description:
+      "고객에게 안정적이고 유연한 서비스를 제공하기 위해, 적극적으로 새로운 기술을 도입해 보면서 해답을 찾고 있는 소프트웨어 엔지니어입니다.",
+    thumbnail: leejun,
+    link: "https://www.linkedin.com/in/jun-lee-8530b4210",
+  },
   nhs: {
-    name: "나현식", 
-    team: "Media솔루션개발팀", 
-    description: "Cloud Streaming과 가상화 기술을 통해 보다 나은 서비스를 제공하고자 새로운 기술과 가치를 만들어가는 엔지니어입니다.", 
-    thumbnail: nhs, 
-  }, 
+    name: "나현식",
+    team: "Media솔루션개발팀",
+    description:
+      "Cloud Streaming과 가상화 기술을 통해 보다 나은 서비스를 제공하고자 새로운 기술과 가치를 만들어가는 엔지니어입니다.",
+    thumbnail: nhs,
+  },
   kyuho: {
-    name: "황규호", 
-    team: "IoT개발팀", 
-    description: "다양한 센서로 취득한 방대한 양의 데이터를 AI를 활용하여 보다 더 가치 있는 정보로 변환하기 위해 노력하고 있습니다.", 
-    thumbnail: kyuho, 
-  }, 
+    name: "황규호",
+    team: "IoT개발팀",
+    description:
+      "다양한 센서로 취득한 방대한 양의 데이터를 AI를 활용하여 보다 더 가치 있는 정보로 변환하기 위해 노력하고 있습니다.",
+    thumbnail: kyuho,
+  },
 };
