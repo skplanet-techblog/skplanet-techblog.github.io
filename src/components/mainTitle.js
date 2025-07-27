189f23ae15fc1bdbd781280a9a60f302dc9a7849
Joe <nocoder100@gmail.com>
2025-07-28 01:13:06 +0900

    fix title

--- src/components/mainTitle.js
+++ src/components/mainTitle.js
@@ -4,7 +4,7 @@ const MainTitle = () => (
   <div className="main-title mt-4 text-center">
     <div className="font-bold leading-tight text">
       <div>
-        SK플래닛의 개발 활동을 공유하는 <span className="highlight">기술 블로그</span>입니다.
+        SK플래닛의 <span className="highlight">개발자 활동</span>을 공유하는 블로그입니다.
       </div>
     </div>
   </div>
