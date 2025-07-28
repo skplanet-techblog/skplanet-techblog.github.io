3f48ee8c37d2ff1eb7e4c7deaa0d6dc84e8cfbbb
Joe <nocoder100@gmail.com>
2025-07-28 17:26:49 +0900

    accessibility9

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,6 +9,27 @@
  */
 const React = require("react");
 
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents, getPostBodyComponents, replacePostBodyComponents }) => {
+  const headComponents = getHeadComponents();
+  const postBodyComponents = getPostBodyComponents();
+
+  // 예: 불필요한 class=""를 제거하는 예시 (필요시 확장)
+  const clean = component => {
+    if (
+      typeof component === "object" &&
+      component.props &&
+      component.props.className === ""
+    ) {
+      delete component.props.className;
+    }
+    return component;
+  };
+
+  replaceHeadComponents(headComponents.map(clean));
+  replacePostBodyComponents(postBodyComponents.map(clean));
+};
+
+
 exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: "en" });
 
